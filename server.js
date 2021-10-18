eb9ceb68bb980dca1dd85b255389daadb6ad3c64
Melchor Ramos <mel.daniel.ramos@gmail.com>
2021-10-18 00:19:11 -0500

    server change 1

--- server.js
+++ server.js
@@ -47,17 +47,17 @@ app.get("/api/waitlist", function(req, res) {
 app.post("/api/tables", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    var newtable = req.body;
+    var newReservation = req.body;
   
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
   
-    console.log(newtable);
+    console.log(newReservation);
   
-    characters.push(newtable);
+    characters.push(newReservation);
   
-    res.json(newtable);
+    res.json(newReservation);
   });
 
 // Add to waiting list - takes in JSON input
