ef2d79115ef0612b2c0d850d80247434033a7a66
Melchor Ramos <mel.daniel.ramos@gmail.com>
2021-10-18 00:06:51 -0500

    server2

--- docs/server.js
+++ docs/server.js
@@ -11,3 +11,75 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//set up variables
+var tables =[];
+
+var waitlist =[];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  // Displays Tables
+app.get("/api/tables", function(req, res) {
+    return res.json(characters);
+  });
+
+  // Displays waiting list
+app.get("/api/waitlist", function(req, res) {
+    return res.json(characters);
+  });
+
+// Reserve table - takes in JSON input
+app.post("/api/tables", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newtable = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newtable);
+  
+    characters.push(newtable);
+  
+    res.json(newtable);
+  });
+
+// Add to waiting list - takes in JSON input
+app.post("/api/waitlist", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newwait = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newtable.routeName = newwait.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newwait);
+  
+    characters.push(newwait);
+  
+    res.json(newwait);
+  });
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
